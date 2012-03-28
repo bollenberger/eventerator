6b73a358f9840ff142b1ddac5c97f268ca269fc9
Brian Ollenberger <brian@ollenberger.com>
2012-03-28 13:51:48 -0700

    Overriding some web server builtins for HttpSapi.

--- http_sapi.php
+++ http_sapi.php
@@ -2,37 +2,48 @@
 
 require_once('http.php');
 
-class AsyncHttpSapi extends Request {
+class HttpSapi extends Request {
     public static $REQUEST;
-    public static $EVENTS;
     
     function set_request_globals() {
         self::$REQUEST = $this;
-        self::$EVENTS = $this->events;
-    }
-    
-    static function override_functions() {
-        $functions = array (
-            'header' => '$string, $replace = true, $http_response_code = null',
-            'header_remove' => '$name',
-            'headers_list' => '',
-            'headers_sent' => '&$file = null, &$line = null',
-            'setcookie' => '$name, $value = null, $expire = 0, $path = null, $domain = null, $secure = false, $httponly = false',
-            'setrawcookie' => '$name, $value = null, $expire = 0, $path = null, $domain = null, $secure = false, $httponly = false',
-            'php_sapi_name' => '',
-            );
-        foreach ($functions as $name => $args) {
-            //override_function($name, $args, "return call_user_func_array(array(AsyncHttpSapi::\$REQUEST, '$name'), func_get_args());");
-            //rename_function('__overridden__', '__' . $name . '_asyncsapi_overridden__');
-        }
     }
     
     function respond() {
     }
+    
+    function abort() {
+    }
+    
+    // Overridden functions
+    static function header($string, $replace = true, $http_response_code = null) {
+    }
+    
+    static function header_remove($name) {
+    }
+    
+    static function headers_list() {
+    }
+    
+    static function headers_sent(&$file = null, &$line = null) {
+    }
+    
+    static function setcookie($name, $value = null, $expire = 0, $path = null, $domain = null, $secure = false, $httponly = false) {
+    }
+    
+    static function setrawcookie($name, $value = null, $expire = 0, $path = null, $domain = null, $secure = false, $httponly = false) {
+    }
+    
+    static function php_sapi_name() {
+        return 'async_http';
+    }
+    
+    // Start server
+    static function start($callback, $application = null, $address = 'tcp://127.0.0.1:8001') {
+        HttpServer($address, self, array($callback, $application));
+        Events::yield();
+    }
 }
 
-AsyncHttpSapi::override_functions();
-
-$events = new Events();
-new HttpServer($events, 'tcp://127.0.0.1:8001', 'AsyncHttpSapi', 'application');
-$events->run();
+$php_sapi_name = 'php_sapi_name';
+echo $$php_sapi_name();
--- http_sapi_plugin.php
+++ http_sapi_plugin.php
@@ -3,12 +3,24 @@
 // for each static method in HttpSapi, create a builtin that calls that function
 
 foreach (array(
-    'headers'
+    'header',
+    'header_remove',
+    'headers_list',
+    'headers_sent',
+    'setcookie',
+    'setrawcookie',
+    'php_sapi_name'
     ) as $name)
 {
     $GLOBALS['STATIC_FUNCTION_TRANSFORM'][$name] = function ($function, $args, $state) {
         array_unshift($args, $state->generateExceptParameter());
         array_unshift($args, new PHPParser_Node_Expr_Variable(CONT_NAME));
         return array(new PHPParser_Node_Stmt_Return(new PHPParser_Node_Expr_StaticCall(new PHPParser_Node_Name('HttpSapi'), $function, $args)));
+    };
+    
+    $GLOBALS['BUILTIN_FUNCTIONS'][$name] = function ($f, $args, $c, $x) {
+        array_unshift($args, $x);
+        array_unshift($args, $c);
+        return call_user_func_array(array('HttpSapi', $f), $args);
     };
 }
